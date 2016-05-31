acf0fb8cc29869b5abe6e384aa8c4d4a58e0ba87
rivasd <rivasdaniel1992@gmail.com>
2016-05-31 12:38:50 -0400

    changed content script to pure js to avoid loading jquery evry time

--- app/detectjspsych.js
+++ app/detectjspsych.js
@@ -3,7 +3,7 @@
  * Detects if the special "jspsyc" attribute has been set on the body element, indicating that jspsych has been loaded
  */
 function detectJsPsych(){
-	var presence = $("html").attr("jspsych");
+	var presence = document.documentElement.getAttribute("jspsych")
 	if(presence === 'present'){
 		chrome.runtime.sendMessage('jspsych-detected');
 	}
