2fbc5bd5cc7119d9570af01c1c07fb2fd722d14f
rivasd <rivasdaniel1992@gmail.com>
2017-11-16 15:28:57 -0500

    removing useless host folder

--- hosts/unixwrapper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Our Python host that controls the hardwares needs root privileges to access the ports. I will try a very dumb approach and simply
-# call this one liner instead of directly calling th .py, so that I can sudo
-
-sudo ./hostmessenger.py
